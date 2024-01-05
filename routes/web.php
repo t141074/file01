0840086ebfe656532a720851f4194b9c694cb3cd
t141074 <t141074@wakayama-u.ac.jp>
2024-01-05 05:02:29 +0000

    1月5日カリキュラム解答17に沿って更新” git commit -m 1月5日カリキュラム解答17に沿って更新”

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', function () {
-    return view('posts/index');
-});
+Route::get('/', [PostController::class, 'index'])
+Route::get('/posts/{post}', [PostController::class, 'show']);
