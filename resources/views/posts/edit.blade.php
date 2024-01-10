9d5908389e0c7c544cf1481a98ac0e4e5491c488
t141074 <t141074@wakayama-u.ac.jp>
2024-01-10 10:40:56 +0000

    8-5終了後更新

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -3,29 +3,30 @@
     <head>
         <meta charset="utf-8">
         <title>Blog</title>
-        
-        
-        
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
         
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <form action="/posts/{{ $post->id }}" method="POST">
             @csrf
+            @method('PUT')
             <div class="title">
                 <h2>Title</h2>
-                <input type="text" name=post[title] placeholder="タイトル" value={{ old('post.title' )}}>
+                <input type="text" name=post[title] placeholder="タイトル" value={{ $post->title }}>
                 <p class='title__error' style="color:blue">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{old('post.body')}}</textarea>
+                <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{ $post->body }}</textarea>
                 <p class="body__error" style="color:blue">{{ $errors->first('post.body') }}</p>
             </div>
-            <input type="submit" value="store"/>
+            <input type="submit" value="update"/>
         </form>
         <div class="footer">
-            <a href="/">戻る</a>
+            <a href="/posts/{{ $post->id }}">戻る</a>
         </div>
     </body>
 </html>
