4b157fcd5cbb972b8b01174ce84bcbd9e6ce3c5f
t141074 <t141074@wakayama-u.ac.jp>
2024-01-12 09:53:26 +0000

    8-6終了後更新

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,7 @@
         <title>Blog</title>
 
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
     </head>
     <body class="antialiased">
@@ -18,10 +18,24 @@
                     <h2 class='title'>
                     <a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
                     <p class='body'>{{ $post->body }}</p>
+                    <form action="/posts/{{ $post->id }}" id="form_{{ $post->id }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <button type="button" onclick="deletePost({{ $post->id }})">delete</button>
+                    </form>
                 </div>
                 @endforeach
         </div>
         <div class='paginate'>{{ $posts->links()}}</div>
+        <script>
+            function deletePost(id) {
+                'use strict'
+                
+                if(confirm('削除すると復元できません。\n本当に削除しますか？')){
+                    document.getElementById(`form_${id}`).submit();
+                }
+            }
+        </script>
     </body>
 </html>
 
